e28b57d5372764d8de6c25be9f056fd743d4cb4d
Steven Choi <steven.m.choi@gmail.com>
2018-02-23 16:40:03 -0800

    Create hash functions for Array, String, and Hash

--- hash_map_lru_cache/lib/p02_hashing.rb
+++ hash_map_lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,25 @@ end
 
 class Array
   def hash
+    result = 0.hash
+
+    each_with_index do |num, idx|
+      result ^=  num.hash * idx.hash
+    end
+
+    result
   end
 end
 
 class String
   def hash
+    str_arr = []
+
+    chars do |chr|
+      str_arr << chr.ord
+    end
+
+    str_arr.hash
   end
 end
 
@@ -16,6 +30,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    result = 0.hash
+
+    each do |key, val|
+      result ^= key.to_s.hash * val.hash
+    end
+
+    result
   end
 end
